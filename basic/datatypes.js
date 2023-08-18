fdcb77a238f1fb593ddca756786b3ce86b23ae61
madhu8585 <142588998+madhu8585@users.noreply.github.com>
2023-08-18 12:52:20 +0000

    data tyoe variable

---
+++ basic/datatypes.js
@@ -0,0 +1,9 @@
+let name="hitesh"
+let age=17
+let isLoggedIn=false
+//number
+
+//bigint
+//string""
+//boolean, null, undefined,symbol,object,,
+
---
+++ basic/variable.js
@@ -0,0 +1,7 @@
+const accountId=12543
+let emailId="nav@gmail.com"
+var accountPassword="654321"
+accountCity="kushinagar"
+
+
+console.table([accountId,emailId,accountPassword,accountCity])
