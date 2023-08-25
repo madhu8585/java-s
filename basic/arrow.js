7c755a8e5ea1f1a8c5bf31f8d74766cf9cc1b9f9
madhu8585 <142588998+madhu8585@users.noreply.github.com>
2023-08-25 18:00:20 +0000

    arrows

---
+++ basic/arrow.js
@@ -0,0 +1,44 @@
+const user = {
+    username:"hit",
+    price:99,
+
+    welmes:function(){///this  kewords is used for current context in the scope variables
+        console.log(`${this.username}, welcome to website`);
+        //console.log(this);
+    }
+
+}
+//user.welmes()
+//user.username="sam"
+//user.welmes()
+
+//function chai (){
+  // let username="madhukar" 
+    //console.log(this.username);
+//}
+//chai()
+
+//const chai =function (){
+ //   let username="madhukar" 
+ //     console.log(this.username);
+ // }
+
+
+ // const chai = () => {
+ //   let username="madhukar" 
+ //     console.log(this);
+ // }
+ // chai()
+
+ //const addtwo=(num1, num2)=>{
+ //   return num1+num2
+ //}
+ //console.log (addtwo(2,4));
+
+ //const addtwo=(num1, num2)=>  num1 + num2
+
+
+ //const addtwo=(num1, num2)=>  (num1 + num2)
+ const addtwo=(num1, num2)=>({username:"madh"})
+ //console.log(addtwo(7,4));
+
